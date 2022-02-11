86b5f984a7fdd2fd9c02d9e8640aaf4d4d22cfd7
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-02-11 16:32:55 +0100

    fix: builds

--- apps/client/src/components/debounce.tsx
+++ apps/client/src/components/debounce.tsx
@@ -1,7 +1,10 @@
-export function debounce(fn, wait = 100) {
-    let timeout;
-    return function (...args) {
+import { AnyFunction } from "@pastable/core";
+
+export function debounce(fn: AnyFunction, wait = 100) {
+    let timeout: any;
+    return function (...args: any[]) {
         clearTimeout(timeout);
+        // @ts-ignore
         timeout = setTimeout(() => fn.call(this, ...args), wait);
     };
 }
